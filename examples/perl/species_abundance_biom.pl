a0c0b72688627e7fdfb899ea6490399b4e94d009
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2013-11-07 15:23:03 -0600

    take arguments from command line

--- examples/perl/species_abundance_biom.pl
+++ examples/perl/species_abundance_biom.pl
@@ -8,13 +8,22 @@ use Data::Dumper;
 my $ua = LWP::UserAgent->new;
 $ua->agent("MyClient/0.1 ");
 
+
 # set the parameters
-my $metagenome = "mgm4440026.3";
-my $source = "GenBank";
+my $key = $ENV{"MGRKEY"};
+my $metagenome = $ARGV[0];
+my $source = $ARGV[1];
+
+die "Metagenome ID and source arguments are required!
+Usage: species_abundance_biom.pl <mgr accession number> <source>
+Example: species_abundance_biom.pl mgm4440026.3 GenBank\n " unless $#ARGV + 1 == 2;
+die "Don't recognize format of $metagenome" unless $metagenome =~ m/\d\d\d\d\d\d\d.\d/;
+
 
 # retrieve the data
 my $base_url = "http://api.metagenomics.anl.gov/1/matrix/organism";
-my $url=$base_url.uri_escape("?id=$metagenome&source=$source");
+my $url=$base_url.uri_escape(defined($key)?"?auth=$key&":"?").uri_escape("id=$metagenome&source=$source");
+print "url: $url\n";
 my $content = $ua->get($url)->content;
 
 # create perl data structure from json
