69babcbbb3e310eba2e1e5ccbad01a93e8f23d8d
Travis Harrison <guumby@gmail.com>
2013-11-25 16:04:39 -0600

    new scripts for group stuff

--- tools/bin/mg-changing-annotation.py
+++ tools/bin/mg-changing-annotation.py
@@ -9,13 +9,13 @@ from mglib import *
 
 prehelp = """
 NAME
-    mg-group-significance
+    mg-changing-annotation
 
 VERSION
     %s
 
 SYNOPSIS
-    mg-group-significance [ --help, --user <user>, --passwd <password>, --token <oAuth token>, --input <input file or stdin>, --format <cv: 'text' or 'biom'>, --groups <json string or filepath>, --plot <filename for pdf>, --rlib <R lib path>, --stat_test <cv: Kruskal-Wallis, t-test-paired, Wilcoxon-paired, t-test-unpaired, Mann-Whitney-unpaired-Wilcoxon, ANOVA-one-way>, --order <column number>, --direction <cv: 'asc', 'desc'> ]
+    mg-changing-annotation [ --help, --user <user>, --passwd <password>, --token <oAuth token>, --input <input file or stdin>, --format <cv: 'text' or 'biom'>, --groups <json string or filepath>, --rlib <R lib path>, --top <N lines to return>, --stat_test <cv: Kruskal-Wallis, t-test-paired, Wilcoxon-paired, t-test-unpaired, Mann-Whitney-unpaired-Wilcoxon, ANOVA-one-way> ]
 
 DESCRIPTION
     Tool to apply matR-based statistical tests to grouped metagenomic abundace profiles.
@@ -30,10 +30,10 @@ Input
        ie. {"group1": ["mg_id_1", "mg_id_3"], "group2": ["mg_id_2", "mg_id_4"], "group3": ["mg_id_5", "mg_id_6", "mg_id_7"]}
 
 Output
-    Tab-delimited table of input abundance profiles with significance statistics based on input groups.
+    Tab-delimited table of input abundance profiles with significance statistics ordered by most significant (changing)
 
 EXAMPLES
-    mg-compare-taxa --ids 'mgm4441679.3,mgm4441680.3,mgm4441681.3,mgm4441682.3' --level class --source RefSeq --format text | mg-group-significance --input - --format text --groups '{"group1":["mgm4441679.3","mgm4441680.3"],"group2":["mgm4441681.3","mgm4441682.3"]}' --stat_test Kruskal-Wallis --direction asc
+    mg-compare-taxa --ids 'mgm4441679.3,mgm4441680.3,mgm4441681.3,mgm4441682.3' --level class --source RefSeq --format text | mg-changing-annotation --input - --format text --groups '{"group1":["mgm4441679.3","mgm4441680.3"],"group2":["mgm4441681.3","mgm4441682.3"]}' --top 5 --stat_test Kruskal-Wallis
 
 SEE ALSO
     -
@@ -52,11 +52,9 @@ def main(args):
     parser.add_option("", "--input", dest="input", default='-', help="input: filename or stdin (-), default is stdin")
     parser.add_option("", "--format", dest="format", default='text', help="input format: 'text' for tabbed table, 'biom' for BIOM format, default is text")
     parser.add_option("", "--groups", dest="groups", default=None, help="groups in JSON format - either as input string or filename")
-    parser.add_option("", "--plot", dest="plot", default=None, help="filename for output plot, optional")
     parser.add_option("", "--rlib", dest="rlib", default=None, help="R lib path")
+    parser.add_option("", "--top", dest="top", default=10, help="display only the top N most changing groups, default is 10")
     parser.add_option("", "--stat_test", dest="stat_test", default='Kruskal-Wallis', help="supported statistical tests, one of: Kruskal-Wallis, t-test-paired, Wilcoxon-paired, t-test-unpaired, Mann-Whitney-unpaired-Wilcoxon, ANOVA-one-way, default is Kruskal-Wallis")
-    parser.add_option("", "--order", dest="order", default=None, help="column number to order output by, default is last column")
-    parser.add_option("", "--direction", dest="direction", default="asc", help="direction of order. 'asc' for ascending order, 'desc' for descending order, default is asc")
     
     # get inputs
     (opts, args) = parser.parse_args()
@@ -68,9 +66,6 @@ def main(args):
         return 1
     if (not opts.rlib) and ('R_LIBS' in os.environ):
         opts.rlib = os.environ['R_LIBS']
-    if opts.direction not in ['asc', 'desc']:
-        sys.stderr.write("ERROR: invalid sort direction\n")
-        return 1
     
     # get auth
     token = get_auth_token(opts)
@@ -123,27 +118,25 @@ def main(args):
                 return 1
     
     # build R cmd
-    fig_out    = '"%s"'%opts.plot if opts.plot else 'NULL'
-    order_by   = 'NULL' if opts.order is None else int(opts.order)
-    order_desc = 'TRUE' if opts.direction == 'desc' else 'FALSE'
     group_str  = 'c('+','.join(map(lambda x: '"%s"'%x, groups))+')'
     r_cmd = """source("%s/group_stats_plot.r")
 suppressMessages( group_stats_plot(
     file_in="%s",
     file_out="%s",
-    figure_out=%s,
+    figure_out=NULL,
     stat_test="%s",
-    order_by=%s,
-    order_decreasing=%s,
+    order_by=NULL,
+    order_decreasing=TRUE,
     my_grouping=%s
-))"""%(opts.rlib, tmp_in, tmp_out, fig_out, opts.stat_test, order_by, order_desc, group_str)
+))"""%(opts.rlib, tmp_in, tmp_out, opts.stat_test, group_str)
     execute_r(r_cmd)
     
     # output results
-    results = open(tmp_out, 'r').read()
+    results = open(tmp_out, 'r').readlines()
+    output  = "\n".join(results[0:opts.top+1])
     os.remove(tmp_in)
     os.remove(tmp_out)
-    safe_print(results)
+    safe_print(output)
     
     return 0
     
