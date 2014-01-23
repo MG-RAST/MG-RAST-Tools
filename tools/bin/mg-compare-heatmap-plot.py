983cce25816b796b941fd1c1b361897a82e6d037
Travis Harrison <guumby@gmail.com>
2014-01-23 12:09:18 -0600

    added heatmap options

--- tools/bin/mg-compare-heatmap-plot.py
+++ tools/bin/mg-compare-heatmap-plot.py
@@ -14,7 +14,7 @@ VERSION
     %s
 
 SYNOPSIS
-    mg-compare-heatmap-plot [ --help, --input <input file or stdin>, --format <cv: 'text' or 'biom'>, --plot <filename for png>, --rlib <R lib path>, --height <image height in inches> --width <image width in inches> --dpi <image DPI> --order <boolean>, --label <boolean> ]
+    mg-compare-heatmap-plot [ --help, --input <input file or stdin>, --format <cv: 'text' or 'biom'>, --plot <filename for png>, --cluster <cv: ward, single, complete, mcquitty, median, centroid>, --distance <cv: bray-curtis, euclidean, maximum, manhattan, canberra, minkowski, difference>, --rlib <R lib path>, --height <image height in inches> --width <image width in inches> --dpi <image DPI> --order <boolean>, --label <boolean> ]
 
 DESCRIPTION
     Tool to generate heatmap-dendrogram vizualizations from metagenome abundance profiles.
@@ -46,6 +46,8 @@ def main(args):
     parser.add_option("", "--input", dest="input", default='-', help="input: filename or stdin (-), default is stdin")
     parser.add_option("", "--format", dest="format", default='text', help="input format: 'text' for tabbed table, 'biom' for BIOM format, default is text")
     parser.add_option("", "--plot", dest="plot", default=None, help="filename for output plot")
+    parser.add_option("", "--cluster", dest="cluster", default='ward', help="cluster function, one of: ward, single, complete, mcquitty, median, centroid, default is ward")
+    parser.add_option("", "--distance", dest="distance", default='bray-curtis', help="distance function, one of: bray-curtis, euclidean, maximum, manhattan, canberra, minkowski, difference, default is bray-curtis")
     parser.add_option("", "--rlib", dest="rlib", default=None, help="R lib path")
     parser.add_option("", "--height", dest="height", type="float", default=5, help="image height in inches, default is 5")
     parser.add_option("", "--width", dest="width", type="float", default=4, help="image width in inches, default is 4")
