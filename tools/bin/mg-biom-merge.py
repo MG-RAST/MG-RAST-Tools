ab389c6be0f53e61f3ed00b62a7748502607ce9a
Jared bischof <jbischof@gmail.com>
2014-08-14 09:50:28 -0500

    Added option to retain duplicate IDs when merging bioms.

--- tools/bin/mg-biom-merge.py
+++ tools/bin/mg-biom-merge.py
@@ -14,7 +14,7 @@ VERSION
     %s
 
 SYNOPSIS
-    mg-biom-merge [ --help ] biom1 biom2 [ biom3 biom4 ... ]
+    mg-biom-merge [ --help --retain_dup_ids ] biom1 biom2 [ biom3 biom4 ... ]
 
 DESCRIPTION
     Tool to merge two or more BIOM format files
@@ -41,6 +41,7 @@ def main(args):
     OptionParser.format_description = lambda self, formatter: self.description
     OptionParser.format_epilog = lambda self, formatter: self.epilog
     parser = OptionParser(usage='', description=prehelp%VERSION, epilog=posthelp%AUTH_LIST)
+    parser.add_option("", "--retain_dup_ids", dest="retain_dups", action="store_true", default=False, help="append input number to duplicate input ID's rather than discarding duplicates, default is false")
 
     # get inputs
     (opts, args) = parser.parse_args()
@@ -57,7 +58,12 @@ def main(args):
     except:
         sys.stderr.write("ERROR: %s BIOM data not correct format\n"%args[0])
         return 1
-    
+
+    input_num = 1
+    if opts.retain_dups:
+        for index in range(len(biom['columns'])):
+            biom['columns'][index]['id'] = biom['columns'][index]['id'] + "_" + str(input_num)
+
     # merge rest
     for f in args[1:]:
         try:
@@ -65,6 +71,12 @@ def main(args):
         except:
             sys.stderr.write("ERROR: %s BIOM data not correct format\n"%f)
     	    return 1
+
+        if opts.retain_dups:
+            input_num += 1
+            for index in range(len(b['columns'])):
+                b['columns'][index]['id'] = b['columns'][index]['id'] + "_" + str(input_num)
+
     	biom = merge_biom(biom, b)
     
     safe_print(json.dumps(biom)+"\n")
