3d8588c28eb39bb5d3332739a4f25d53b25586a6
Will Trimble <trimble@anl.gov>
2018-01-25 17:27:22 -0600

    implementing more api parameters

--- examples/python/abundance_matrix.py
+++ examples/python/abundance_matrix.py
@@ -17,9 +17,10 @@ if __name__ == '__main__':
     parser.add_option("-g", "--grouplevel", dest="grouplevel", default="domain", help="Grouping level: strain, species, genus, family, order, class, phylum, domain / function, level1, level2, level3")
     parser.add_option("-i", "--hittype", dest="hittype", default="single", help="Hit type: all, single, lca")
     parser.add_option("-c", "--call", dest="call", default="organism", help="organism or function")
+    parser.add_option("-d", "--identity", dest="identity", default=1, help="% identity threshold")
     parser.add_option("-e", "--evalue", dest="evalue", default="1", help="organism or function")
     parser.add_option("-t", "--type", dest="resulttype", default="abundance", help="Result type: abundnaance, evalue, identity, or length")
-    parser.add_option("-n", "--ngth", dest="length", default="15", help="leNgth of hit")
+    parser.add_option("-n", "--ngth", dest="length", default="1", help="leNgth of hit")
 #    parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=True, help="Verbose [default off]")
     parser.add_option("-k", "--token", dest="token", type="str", help="Auth token")
     parser.add_option("-m", "--metagenomes", dest="metagenomes", default="mgm4447943.3,mgm4447102.3", type="str", help="Metagenome list")
@@ -33,14 +34,17 @@ if __name__ == '__main__':
     result_type = opts.resulttype
     source = opts.source
     evalue = opts.evalue
-
+    length = opts.length
+    identity = opts.identity
+    hittype = opts.hittype
 # construct API call
     base_url = API_URL + "/matrix/organism"
     if opts.call == "function" or opts.source == "SubSystems":
         base_url = API_URL + "/matrix/function"
-    base_url = base_url + "?asynchronous=1&group_level=%s&result_type=%s&auth=%s&source=%s&evalue=%s&" % (group_level, result_type, key, source, evalue)
+    base_url = base_url + "?asynchronous=1&group_level=%s&result_type=%s&auth=%s&source=%s&evalue=%s&length=%s&identity=%s&hittype=%s&" % (group_level, result_type, key, source, evalue, length, identity, hittype)
     URI = base_url + "&".join(["id=%s" % m for m in metagenomes.split(",")])
     print(URI, file=sys.stderr)
+    print("#"+ URI, file=sys.stdout)
 # retrieve the data by sending at HTTP GET request to the MG-RAST API
 
     jsonstructure = async_rest_api(URI, auth=key)
