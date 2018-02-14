b636ad12bedee0fbbd66397b48a2cec6a8a2ce19
Will Trimble <trimble@anl.gov>
2018-02-14 11:27:35 -0600

    Change API URI to api.mg-rast.org

--- mglib/__init__.py
+++ mglib/__init__.py
@@ -1,7 +1,7 @@
 import os
 auth_file = os.path.join(os.path.expanduser('~'), ".mgrast_auth")
 VERSION = '1'
-API_URL = "http://api.metagenomics.anl.gov/"+VERSION
+API_URL = "https://api.mg-rast.org/"+VERSION
 SHOCK_URL = "http://shock.metagenomics.anl.gov"
 AUTH_LIST = "Jared Bischof, Travis Harrison, Folker Meyer, Tobias Paczian, Andreas Wilke"
 SEARCH_FIELDS = ["function", "organism", "md5", "name", "metadata", "biome", "feature", "material", "country", "location", "longitude", "latitude", "created", "env_package_type", "project_id", "project_name", "PI_firstname", "PI_lastname", "sequence_type", "seq_method", "collection_date"]
--- mglib/mglib.py
+++ mglib/mglib.py
@@ -36,8 +36,8 @@ if not sys.version_info[0:2][0] == 3 and not sys.version_info[0:2] == (2, 7) :
 
 auth_file = os.path.join(os.path.expanduser('~'), ".mgrast_auth")
 VERSION = '1'
-API_URL = "http://api.metagenomics.anl.gov/"+VERSION
-SHOCK_URL = "http://shock.metagenomics.anl.gov"
+API_URL = "https://api.mg-rast.org/"+VERSION
+SHOCK_URL = "https://shock.metagenomics.anl.gov"
 AUTH_LIST = "Jared Bischof, Travis Harrison, Folker Meyer, Tobias Paczian, Andreas Wilke"
 SEARCH_FIELDS = ["function", "organism", "md5", "name", "metadata", "biome", "feature", "material", "country", "location", "longitude", "latitude", "created", "env_package_type", "project_id", "project_name", "PI_firstname", "PI_lastname", "sequence_type", "seq_method", "collection_date"]
 
