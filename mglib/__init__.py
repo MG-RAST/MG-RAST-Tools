debf2e0f719680be5ed8193ed1993beafca32de6
Travis Harrison <guumby@gmail.com>
2018-10-29 11:14:09 -0500

    update search script

--- mglib/__init__.py
+++ mglib/__init__.py
@@ -9,4 +9,4 @@ VERSION = 1
 API_URL = "https://api.mg-rast.org/"
 SHOCK_URL = "https://shock.mg-rast.org/"
 AUTH_LIST = "Jared Bischof, Travis Harrison, Folker Meyer, Tobias Paczian, Andreas Wilke"
-SEARCH_FIELDS = ["name", "investigation_type", "biome", "feature", "material", "continent", "country", "location", "longitude", "latitude", "created_on", "env_package", "project_id", "project_name", "PI_firstname", "PI_lastname", "sequence_type", "seq_meth", "collection_date", "bp_count_raw", "sequence_count_raw", "average_length_raw"]
+SEARCH_FIELDS = ["all", "name", "investigation_type", "biome", "feature", "material", "continent", "country", "location", "longitude", "latitude", "created_on", "env_package", "project_id", "project_name", "PI_firstname", "PI_lastname", "sequence_type", "seq_meth", "collection_date", "bp_count_raw", "sequence_count_raw", "average_length_raw", "taxonomy", "function"]
